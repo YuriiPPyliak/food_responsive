a64ac9f00d459cf702faf57a8f4a816e4455ee6e
Yurii <jerzykagh@gmai.com>
2023-01-12 00:16:32 +0200

    adding transition to accordion and active buttton to slick slader

--- script/script.js
+++ script/script.js
@@ -20,6 +20,7 @@ $('.slider').slick({
 		]
 });
         
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -27,10 +28,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
